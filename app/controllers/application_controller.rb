1c4b91c0095d4c78141e6c138ed01603c248c9d9
uchigakiDaiju <uchigaki.daiju@gmail.com>
2020-10-02 21:34:36 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller? #deviseのコントローラーから呼び出された場合
+ #application_controllerにbefore_acitonを使っているので、全てのアクションが、
+ #実行される前に、before_action :configure_permitted_parameters, if: :devise_controller?が実行されることになります。
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end

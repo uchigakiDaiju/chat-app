d7d006886b2c972dc65e04b22a5bc6d2d10d2b2a
uchigakiDaiju <uchigaki.daiju@gmail.com>
2020-10-07 01:24:41 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "rooms#index"
   resources :users, only: [:edit, :update, :destroy]
-  resources :rooms, only: [:new, :create, :index]
+  resources :rooms, only: [:new, :create ] do
+    resources :messages, only: [:index, :create]
+  end
   
 end
